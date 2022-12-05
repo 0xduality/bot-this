c71e1a7e5fc5b1d6756e1863b927579d12060798
avax moon <avaxtothemoon@gmail.com>
2022-12-04 23:24:15 -0800

    clean up

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.16;
 
 import {Script} from 'forge-std/Script.sol';
 
-import {ERC20token} from "src/tokens/ERC20token.sol";
+import {BotThisERC721} from "src/tokens/Botthis.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
   /// @notice The main script entrypoint
-  /// @return erc20 The deployed contract
-  function run() external returns (ERC20token erc20) {
+  /// @return erc721 The deployed contract
+  function run() external returns (BotThisERC721 erc721) {
     vm.startBroadcast();
-    erc20 = new ERC20token();
+    erc721 = new BotThisERC721("BotThis", "BT");
     vm.stopBroadcast();
   }
 }
---
+++ test/BotThis.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+
+contract BotThisTest is Test {
+    using stdStorage for StdStorage;
+
+    function setUp() public
+    {
+
+    }
+
+    function testSomething() public
+    {
+        
+    }
+
+}
